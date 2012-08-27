26e4efe69cef7fb2b747b0da03b8a24e6ca2483f
Denis Karataev <dskarataev@gmail.com>
2012-08-27 12:42:23 +0400

    исправил опечатку

--- modules/product_ean13_dsk/__openerp__.py
+++ modules/product_ean13_dsk/__openerp__.py
@@ -22,7 +22,7 @@
     "version" : "1.0",
     "author" : "Denis Karataev",
     "category" : "Generic Modules",
-    "depends" : ["product"
+    "depends" : ["product",
                  "product_base_dsk"],
     "init_xml" : [],
     "demo_xml" : [],
