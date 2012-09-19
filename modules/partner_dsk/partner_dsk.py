e16fd20ac9812e14dd5058e1a98d558454e7a2bc
Denis Karataev <dskarataev@gmail.com>
2012-09-19 22:38:29 +0400

    исправление бага с английскими названиями стран в адресах партнера

--- modules/partner_dsk/partner_dsk.py
+++ modules/partner_dsk/partner_dsk.py
@@ -43,4 +43,27 @@ class res_partner(osv.osv):
     }
 res_partner()
 
+class res_partner_address(osv.osv):
+    _name = 'res.partner.address'
+    _inherit = 'res.partner.address'
+
+    def name_get(self, cr, user, ids, context=None):
+        if context is None:
+            context = {}
+        if not len(ids):
+            return []
+        res = []
+        for r in self.read(cr, user, ids, ['name','zip','country_id', 'city','partner_id', 'street'], context):
+            if context.get('contact_display', 'contact')=='partner' and r['partner_id']:
+                res.append((r['id'], r['partner_id'][1]))
+            else:
+                # make a comma-separated list with the following non-empty elements
+                elems = [r['name'], r['country_id'] and r['country_id'][1], r['city'], r['street']]
+                addr = ', '.join(filter(bool, elems))
+                if (context.get('contact_display', 'contact')=='partner_address') and r['partner_id']:
+                    res.append((r['id'], "%s: %s" % (r['partner_id'][1], addr or '/')))
+                else:
+                    res.append((r['id'], addr or '/'))
+        return res
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
