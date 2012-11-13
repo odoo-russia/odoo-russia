c9d9e09144f6708dafffd5ee02c3d3f0ca537ee2
Denis Karataev <dskarataev@gmail.com>
2012-11-13 16:03:19 +0400

    добавил информацию по партнерам в выгрузку

--- xml-rpc/sync_with_1c_buh.py
+++ xml-rpc/sync_with_1c_buh.py
@@ -133,6 +133,96 @@ try:
             xmlProduct.appendChild(xmlProductUom)
 
             xmlProducts.appendChild(xmlProduct)
+
+        xmlPartners = doc.createElement('partners')
+        xmlData.appendChild(xmlPartners)
+
+        partner_ids = sock.execute(db, uid, pwd, 'res.partner', 'search', [], 0, None, None, context)
+
+        fields = ['id', 'name', 'name_official', 'inn', 'kpp', 'okpo', 'contract_num', 'contract_date']
+        partners = sock.execute(db, uid, pwd, 'res.partner', 'read', partner_ids, fields, context)
+        for partner in partners:
+            xmlPartner = doc.createElement('partner')
+            xmlPartner.setAttribute('id', str(partner['id']))
+
+            xmlPartnerName = doc.createElement('name')
+            xmlPartnerNameText = doc.createTextNode(partner['name'])
+            xmlPartnerName.appendChild(xmlPartnerNameText)
+            xmlPartner.appendChild(xmlPartnerName)
+
+            xmlPartnerNameOfficial = doc.createElement('name_official')
+            xmlPartnerNameOfficialText = doc.createTextNode(partner['name_official'])
+            xmlPartnerNameOfficial.appendChild(xmlPartnerNameOfficialText)
+            xmlPartner.appendChild(xmlPartnerNameOfficial)
+
+            xmlPartnerInn = doc.createElement('inn')
+            xmlPartnerInnText = doc.createTextNode(partner['inn'])
+            xmlPartnerInn.appendChild(xmlPartnerInnText)
+            xmlPartner.appendChild(xmlPartnerInn)
+
+            xmlPartnerKpp = doc.createElement('kpp')
+            xmlPartnerKppText = doc.createTextNode(partner['kpp'])
+            xmlPartnerKpp.appendChild(xmlPartnerKppText)
+            xmlPartner.appendChild(xmlPartnerKpp)
+
+            xmlPartnerOkpo = doc.createElement('okpo')
+            xmlPartnerOkpoText = doc.createTextNode(partner['okpo'])
+            xmlPartnerOkpo.appendChild(xmlPartnerOkpoText)
+            xmlPartner.appendChild(xmlPartnerOkpo)
+
+            xmlPartnerContractName = doc.createElement('contract_name')
+            xmlPartnerContractNameText = doc.createTextNode(partner['contract_name'])
+            xmlPartnerContractName.appendChild(xmlPartnerContractNameText)
+            xmlPartner.appendChild(xmlPartnerContractName)
+
+            xmlPartnerContractDate = doc.createElement('contract_date')
+            xmlPartnerContractDateText = doc.createTextNode(partner['contract_date'])
+            xmlPartnerContractDate.appendChild(xmlPartnerContractDateText)
+            xmlPartner.appendChild(xmlPartnerContractDate)
+
+            fields = ['id', 'type', 'zip', 'country', 'state_id', 'city', 'street', 'phone']
+            partner_address_default_ids = sock.execute(db, uid, pwd, 'res.partner.address', 'search',
+                                                       [('type', '=', 'default')], 0, None, None, context)
+            partner_address_actual_ids = sock.execute(db, uid, pwd, 'res.partner.address', 'search',
+                                                       [('type', '=', 'actual')], 0, None, None, context)
+            if partner_address_default_ids:
+                partner_address_default_id = [partner_address_default_ids[0]]
+                partner_address_default = sock.execute(db, uid, pwd, 'res.partner.address', 'read',
+                                                       partner_address_default_id, fields, context)
+                xmlPartnerAddressDefault = doc.createElement('address_default')
+                xmlPartnerAddressDefaultText = doc.createTextNode('%s, %s, %s, %s, %s' %
+                    (partner_address_default['zip'],
+                     partner_address_default['country'] and partner_address_default['country'][1] or '',
+                     partner_address_default['state_id'] and partner_address_default['state_id'][1] or '',
+                     partner_address_default['city'],
+                     partner_address_default['street']))
+                xmlPartnerAddressDefault.appendChild(xmlPartnerAddressDefaultText)
+                xmlPartner.appendChild(xmlPartnerAddressDefault)
+
+            if partner_address_actual_ids:
+                partner_address_actual_id = [partner_address_actual_ids[0]]
+                partner_address_actual = sock.execute(db, uid, pwd, 'res.partner.address', 'read',
+                                                      partner_address_actual_id, fields, context)
+                xmlPartnerAddressActual = doc.createElement('address_actual')
+                xmlPartnerAddressActualText = doc.createTextNode('%s, %s, %s, %s, %s' %
+                    (partner_address_actual['zip'],
+                     partner_address_actual['country'] and partner_address_actual['country'][1] or '',
+                     partner_address_actual['state_id'] and partner_address_actual['state_id'][1] or '',
+                     partner_address_actual['city'],
+                     partner_address_actual['street']))
+                xmlPartnerAddressActual.appendChild(xmlPartnerAddressActualText)
+                xmlPartner.appendChild(xmlPartnerAddressActual)
+
+            partner_phone = ''
+            if partner_address_default_ids and partner_address_default['phone']:
+                partner_phone = partner_address_default['phone']
+            elif partner_address_actual_ids and partner_address_actual['phone']:
+                partner_phone = partner_address_actual['phone']
+            xmlPartnerPhone = doc.createElement('phone')
+            xmlPartnerPhoneText = doc.createTextNode(partner_phone)
+            xmlPartnerPhone.appendChild(xmlPartnerPhoneText)
+            xmlPartner.appendChild(xmlPartnerPhone)
+
         print doc.toprettyxml(indent='  ', encoding='UTF-8')
 except Exception,e:
     if isinstance(e, Fault):
