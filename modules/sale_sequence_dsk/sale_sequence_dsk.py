302ce8620fec92195da608512dbab996a7743c93
Denis Karataev <dskarataev@gmail.com>
2012-12-25 18:56:20 +0400

    добавил модуль для разделения последовательностей заказов продаж в зависимости от типа оплаты

---
+++ modules/sale_sequence_dsk/sale_sequence_dsk.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class sale_order(osv.osv):
+    def copy(self, cr, uid, id, default=None, context=None):
+        if not default:
+            default = {}
+        default.update({
+            'state': 'draft',
+            'invoice_ids': [],
+            'date_confirm': False,
+            'name': self.pool.get('ir.sequence').get(cr, uid, 'sale.order.bank'),
+            })
+        return super(sale_order, self).copy(cr, uid, id, default, context=context)
+
+    def create(self, cr, uid, vals, context=None):
+        if vals.get('name','/')=='/':
+            print 'yes'
+            if vals.get('is_pay_cash'):
+                sale_order_sequence_code = 'sale.order.cash'
+                print 'yes1'
+            else:
+                sale_order_sequence_code = 'sale.order.bank'
+                print 'yes2'
+            vals['name'] = self.pool.get('ir.sequence').get(cr, uid, sale_order_sequence_code) or '/'
+        order =  super(sale_order, self).create(cr, uid, vals, context=context)
+        if order:
+            self.create_send_note(cr, uid, [order], context=context)
+        return order
+
+    _name = 'sale.order'
+    _inherit = 'sale.order'
+    _columns = {
+        'is_pay_cash': fields.boolean('Pay Cash'),
+    }
+sale_order()
