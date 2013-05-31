08d63230196524db4e5d015c97581d3b4ffd42a4
Ivan Kondrashov <ivkond@gmail.com>
2013-05-31 17:50:44 +0400

    Сделал обрезку номеров

--- modules/account_invoice_print_form/acc_inv_code.py
+++ modules/account_invoice_print_form/acc_inv_code.py
@@ -2,6 +2,7 @@
 
 import time
 from openerp.report import report_sxw
+from osv import orm, osv, fields
 
 class acc_inv(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
@@ -11,3 +12,27 @@ class acc_inv(report_sxw.rml_parse):
 report_sxw.report_sxw('report.new_report', 'account.invoice',
                       'account_invoice_print_form/Akt.jrxml',
                       parser=acc_inv)
+
+class account_invoice(osv.osv):
+    def _get_number_only(self, cr, uid, ids, field_name, arg, context):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            if not row.number:
+                raise osv.except_osv('Error!', 'You must confirm invoice!')
+
+            seq_id = self.pool.get('ir.sequence').search(cr, uid, [('code', '=', 'sale.order')])
+            sequence = self.pool.get('ir.sequence').read(cr, uid, seq_id, ['padding', 'active'])[0]
+            if sequence and sequence.get('active'):
+                padding = sequence.get('padding')
+                padding = 0 - int(padding)
+                res[row.id] = row.number[padding:].lstrip('0')
+
+        return res
+
+    _name = 'account.invoice'
+    _inherit = 'account.invoice'
+    _columns = {
+        'number_only': fields.function(_get_number_only, type='char'),
+    }
+account_invoice()
