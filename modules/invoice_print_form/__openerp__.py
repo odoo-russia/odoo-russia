d6becac227f0eecb8ae414740a125dc7cb5b116e
Ivan Kondrashov <ivkond@gmail.com>
2013-06-19 17:09:58 +0400

    Создал форму Счёт-фактуры

---
+++ modules/invoice_print_form/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Invoice Print Form',
+    'version' : '1.0',
+    'category' : 'Extra Reports',
+    'author'  : 'Transparent Technologies Ltd.',
+    'license' : 'AGPL-3',
+    'depends' : ['base', 'jasper_reports', 'l10n_ru', 'account'],
+    'update_xml' : ['invoice_form_data.xml',],
+    'installable': True,
+    'auto_install': False,
+    'description': '''
+This module adds Invoice Print Form.
+============================================================
+    '''
+}
---
+++ modules/invoice_print_form/invoice_form_code.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+import time
+from openerp.report import report_sxw
+from osv import orm, osv, fields
+from openerp.addons.jasper_reports.pytils import numeral
+
+class invoice_form(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(invoice_form, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update( {'time': time,})
+
+report_sxw.report_sxw('report.new_invoice_form_report', 'account.invoice',
+                      'invoice_print_form/invoice.jrxml',
+                      parser=invoice_form)
+
+class account_invoice(osv.osv):
+    def _get_number_only(self, cr, uid, ids, field_name, arg, context):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            if not row.number:
+                raise osv.except_osv('Error!', 'You must confirm invoice!')
+
+            seq_id = self.pool.get('ir.sequence').search(cr, uid, [('code', '=', 'sale.order')])
+            sequence = self.pool.get('ir.sequence').read(cr, uid, seq_id, ['padding', 'active'])[0]
+            if sequence and sequence.get('active'):
+                padding = sequence.get('padding')
+                padding = 0 - int(padding)
+                res[row.id] = row.number[padding:].lstrip('0')
+
+        return res
+
+    def _get_pos_in_words(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            res[row.id] = numeral.in_words(len(row.invoice_line))
+
+        return res
+
+    def _get_price_in_words(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            rubles = numeral.rubles(int(row.amount_total))
+            copek_tmp = round(row.amount_total - int(row.amount_total))
+            copek = numeral.choose_plural(int(copek_tmp), (u"копейка", u"копейки", u"копеек"))
+            res[row.id] = ("%s %02d %s")%(rubles, copek_tmp, copek)
+
+        return res
+
+    def _get_invoices_count(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            res[row.id] = len(row.invoice_line)
+
+        return res
+
+    _name = 'account.invoice'
+    _inherit = 'account.invoice'
+    _columns = {
+        'number_only': fields.function(_get_number_only, type='char'),
+        'price_in_words': fields.function(_get_price_in_words, type='char'),
+        'pos_in_words': fields.function(_get_pos_in_words, type='char'),
+        'invoices_count': fields.function(_get_invoices_count, type='integer'),
+    }
+account_invoice()
+
+class product_uom(osv.osv):
+    _name = 'product.uom'
+    _inherit = 'product.uom'
+    _columns = {
+        'OKEI': fields.integer('Код по ОКЕИ'),
+    }
+product_uom()
+
+class invoice_line(osv.osv):
+    def _get_line_tax(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            res[row.id] = 0
+
+            for line in row.invoice_line_tax_id:
+                res[row.id] += line.amount
+
+        return res
+
+    def _get_tax_total(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            res[row.id] = row.line_tax_amount * row.price_subtotal
+
+        return res
+
+    def _get_taxed_subtotal(self,cr,uid,ids,field,arg,context=None):
+        res = {}
+
+        for row in self.browse(cr, uid, ids, context):
+            res[row.id] = row.line_tax_total + row.price_subtotal
+
+        return res
+
+    _name = 'account.invoice.line'
+    _inherit = 'account.invoice.line'
+    _columns = {
+        'line_tax_amount': fields.function(_get_line_tax, type='double'),
+        'line_tax_total': fields.function(_get_tax_total, type='double'),
+        'line_taxed_subtotal': fields.function(_get_taxed_subtotal, type='double'),
+    }
+
+class res_currency(osv.osv):
+    _name = 'res.currency'
+    _inherit = 'res.currency'
+    _columns = {
+        'code': fields.integer("Code"),
+    }
+
+class product_product(osv.osv):
+    _inherit = 'product.product'
+    _name = 'product.product'
+    _columns = {
+        'country_origin': fields.char('Country of origin', size=64),
+        'country_code': fields.integer('Country code'),
+        'declaration_code': fields.char('Declaration code'),
+    }
