095b5bacfc3e6112f7babfce2b54768a8340fb62
Denis Karataev <dskarataev@gmail.com>
2013-09-03 15:06:17 +0400

    - объединил наш модуль локализации с модулем от CodUP, т.к. его модуль содержит более правильный план счетов (сам не проверял но так говорят) - добавил подробное описание возможностей модуля

--- modules/l10n_ru/__openerp__.py
+++ modules/l10n_ru/__openerp__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -18,27 +18,37 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{   'name': 'Russia - Accounting',
-    'version': '1.0',
+
+{
+    'name': 'Russia - Accounting',
+    'version': '1.2',
     'category': 'Localization/Account Charts',
     'description': """
-This is the base module to manage the accounting chart for Russia in OpenERP.
-""",
-    'author': 'OpenERP Russian Localization Team',
-    'depends': ['base', 'account', 'account_chart'],
-    'init_xml': [],
-    'update_xml': [
-        'data/account.account.type.csv',
+This module is the localization of OpenERP for Russian Federation.
+==============================================================================
+Возможности:
+
+  - План счетов бухгалтерского учёта финансово-хозяйственной деятельности организаций, утверждённый Приказом Минфина РФ от 31.10.2000 года № 94н
+  - План российских налогов
+  - Справочник российских регионов
+  - Справочник российских организационно-правовых форм (ООО, ЗАО, ОАО, ИП)
+  - Дополнительные реквизиты партнеров на вкладке Учет (Официальное наименование, ИНН, КПП, ОКПО, Номер договора, Дата договора, Руководитель, Главный бухгалтер)
+  - Дополнительный реквизит "Корр. счет" для банков
+  - Автоматическая загрузка актуального справочника российских банков (пункт меню Учет-Настройки-Обновить банки, для этого нужно чтобы пользователь имел права финансового менеджера)
+    """,
+    'author': 'OpenERP Russian Localization Team, Transparent Technologies, CodUP',
+    'website': 'https://launchpad.net/~openerp-l10n-ru, http://tterp.ru, http://codup.com',
+    'images': ['images/flag_ru.png'],
+    'depends': ['account', 'account_chart'],
+    'data': [
         'data/account.account.template.csv',
+        'data/account_chart_template.xml',
         'data/account.tax.code.template.csv',
-        'data/l10n_ru_account_chart_template.xml',
-	'data/res.country.state.csv',
-	"data/res.partner.title.csv",
-        "l10n_ru_view.xml",
+        'data/res.country.state.csv',
+		'data/res.partner.title.csv',
+        'l10n_ru_view.xml',
+        'l10n_ru_wizard.xml',
     ],
-    'demo_xml': [],
     'installable': True,
-    'images': [],
 }
-
-
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
