502a057eda9483383cb5108cfab0fbf06b3f8b93
Ivan Kondrashov <ivkond@gmail.com>
2013-09-05 14:09:52 +0400

    Переимновал модуль Счёт-фактуры что бы было легче его искать в списке модулей. Исправил отображение адреса компании и партнёра: теперь если не заполнено какое-то поле, то запятные не отображаются.

--- modules/tt_print_form_schet_factura/__openerp__.py
+++ modules/tt_print_form_schet_factura/__openerp__.py
@@ -1,5 +1,5 @@
 {
-    'name' : 'Invoice Print Form',
+    'name' : 'Schet Factura Print Form',
     'version' : '1.0',
     'category' : 'Extra Reports',
     'author'  : 'Transparent Technologies',
