f00d0472724a525b2a4af56b47cdac11e134d61e
Denis Karataev <dskarataev@gmail.com>
2013-09-07 13:10:34 +0400

    добавил модуль, который включает налоги в колонку Подитог в заказе на продажу

---
+++ modules/tt_sale_order_line_subtotal_gross/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Gross lines subtotals in Sale Order',
+    'version' : '1.0',
+    'category' : 'Sales Management',
+    'author'  : 'Transparent Technologies',
+    'license' : 'AGPL-3',
+    'depends' : ['base', 'sale'],
+    'installable': True,
+    'auto_install': False,
+    'description': '''
+This modules changes subtotal of line in Sale Order to the gross amount (with tax included)
+    '''
+}
