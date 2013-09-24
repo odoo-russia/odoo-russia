5afac16037b11748dbb57901587f162916fa3977
Ivan Kondrashov <ivkond@gmail.com>
2013-09-24 18:44:25 +0400

    Добавили в зависимости к модулям печатных форм модуль tt_acc_invoice_line_subtotal_gross, а для печатной формы Счёт ещё и модуль tt_sale_order_line_subtotal_gross.

--- modules/tt_print_form_schet_factura/__openerp__.py
+++ modules/tt_print_form_schet_factura/__openerp__.py
@@ -4,7 +4,7 @@
     'category' : 'Extra Reports',
     'author'  : 'Transparent Technologies',
     'license' : 'AGPL-3',
-    'depends' : ['base', 'jasper_reports', 'l10n_ru', 'account'],
+    'depends' : ['base', 'jasper_reports', 'l10n_ru', 'account', 'tt_acc_invoice_line_subtotal_gross'],
     'update_xml' : ['invoice_form_data.xml',],
     'installable': True,
     'auto_install': False,
