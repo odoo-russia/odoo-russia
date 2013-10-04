3d0edffe532139099e16fb0b07c08bac0201fd60
Ivan Kondrashov <ivkond@gmail.com>
2013-10-04 15:39:53 +0400

    Предварительный коммит для задачи #4 Доабавил в account_invoice новые функциональные поля. Все они имеют следующие аргументы: store=False, multi='partner_info' и вызывают одну и ту же функцию - get_partner_info(). Эта функция по названию поля к которому обращаемся определяет какое значение ей возвратить.Сначала по префиксу определяется тип партёра(company_id(Наша компания), partner_id(Заказчик), partner_shipping_id(Грузополучатель) или partner_invoice_id(Покупатель)). Затем по постфиксу поля определяет тип поля(имя, адрес, инн/кпп). Для остальных типов вызывается функция _get_<тип>(). Если такая функция не найдена, то возвращается пустая строка. Данный ход позволяет создавать специфические поля для каждой печатной формы и подставлять их напрямую в отчёты без сложных и запутанных условий в самих формах. В дальнейшем необходимо вынести функцию get_partner_info() в отдельный модуль и прописать его в зависимостях для Счёта-фактуры и Торг-12. Вынос в отдельный модуль нужен для того, чтобы не копировать код по сто раз и что бы это можно было использовать и в следующих печатных фомах.

--- modules/tt_print_form_schet_factura/invoice_form_code.py
+++ modules/tt_print_form_schet_factura/invoice_form_code.py
@@ -2,18 +2,20 @@
 
 import time
 from openerp.report import report_sxw
-from osv import orm, osv, fields
+from openerp.osv import osv, fields
 from openerp.addons.jasper_reports.pytils import numeral
 from tools.translate import _
+
 
 class invoice_form(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(invoice_form, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update( {'time': time,})
+        self.localcontext.update({'time': time})
 
 report_sxw.report_sxw('report.new_invoice_form_report', 'account.invoice',
                       'tt_print_form_schet_factura/invoice.jrxml',
                       parser=invoice_form)
+
 
 class account_invoice(osv.osv):
     def _get_number_only(self, cr, uid, ids, field_name, arg, context):
@@ -32,7 +34,7 @@ class account_invoice(osv.osv):
 
         return res
 
-    def _get_pos_in_words(self,cr,uid,ids,field,arg,context=None):
+    def _get_pos_in_words(self, cr, uid, ids, field, arg, context=None):
         res = {}
 
         for row in self.browse(cr, uid, ids, context):
@@ -40,7 +42,7 @@ class account_invoice(osv.osv):
 
         return res
 
-    def _get_price_in_words(self,cr,uid,ids,field,arg,context=None):
+    def _get_price_in_words(self, cr, uid, ids, field, arg, context=None):
         res = {}
 
         for row in self.browse(cr, uid, ids, context):
@@ -51,12 +53,83 @@ class account_invoice(osv.osv):
 
         return res
 
-    def _get_invoices_count(self,cr,uid,ids,field,arg,context=None):
+    def _get_invoices_count(self, cr, uid, ids, field, arg, context=None):
         res = {}
 
         for row in self.browse(cr, uid, ids, context):
             res[row.id] = len(row.invoice_line)
 
+        return res
+
+    def _get_origin(self, cr, uid, origin_str):
+        sale_obj = self.pool.get('sale.order')
+        sale_ids = sale_obj.search(cr, uid, [('name', '=', origin_str)])
+        if sale_ids:
+            return sale_obj.browse(cr, uid, sale_ids[0])
+        else:
+            return None
+
+    def _format_inn_kpp(self, inn, kpp):
+        if inn and kpp:
+            res = "%s/%s" % (inn, kpp)
+        elif inn:
+            res = inn
+        elif kpp:
+            res = kpp
+        else:
+            res = ""
+        return res
+
+    def _format_bank(self, partner):
+        return "Bank info"
+
+    def get_partner_info(self, cr, uid, ids, fields=None, args=None, context=None):
+        if context is None:
+            context = {}
+        res = {}
+        for i in ids:
+            res[i] = {}.fromkeys(fields, "")
+
+        for field in fields:
+            for invoice in self.browse(cr, uid, ids, context=context):
+                origin = self._get_origin(cr, uid, invoice.origin)
+                field_splitted = field.split('_')
+                field_prefix = field_splitted[0]
+                field_postfix = field_splitted[-1]
+
+                # Get correct partner by the prefix of field name
+                if field_prefix == 'invoice' and origin and origin.partner_invoice_id:
+                    partner = origin.partner_invoice_id
+                elif field_prefix == 'shipping' and origin and origin.partner_shipping_id:
+                    partner = origin.partner_shipping_id
+                elif field_prefix == 'company':
+                    partner = invoice.company_id.partner_id
+                elif field_prefix == 'partner':
+                    partner = invoice.partner_id
+                else:
+                    res[invoice.id][field] = ""
+                    continue
+
+                # Get information by the postfix of field name
+                if field_postfix == 'name':
+                    value = partner.name_official if partner.name_official else partner.name
+                elif field_postfix == 'address':
+                    partner = partner.parent_id if partner.use_parent_address else partner
+                    value = partner.address_formatted
+                elif field_postfix == 'innkpp':
+                    inn = partner.inn if partner.inn else partner.parent_id.inn
+                    kpp = partner.kpp if partner.kpp else partner.parent_id.kpp
+                    value = self._format_inn_kpp(inn, kpp)
+                elif field_postfix == 'bank':
+                    partner = partner.parent_id if partner.is_company else partner
+                    value = self._format_bank(partner)
+                else:
+                    method = getattr(self, "_get_" + field_postfix) or False
+                    if method:
+                        value = method(invoice, partner)
+                    else:
+                        value = ""
+                res[invoice.id][field] = value
         return res
 
     _name = 'account.invoice'
@@ -66,12 +139,21 @@ class account_invoice(osv.osv):
         'price_in_words': fields.function(_get_price_in_words, type='char'),
         'pos_in_words': fields.function(_get_pos_in_words, type='char'),
         'invoices_count': fields.function(_get_invoices_count, type='integer'),
+        'invoice_partner_name': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'invoice_address': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'invoice_innkpp': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'shipping_partner_name': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'shipping_address': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'company_name': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'company_address': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'company_innkpp': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
+        'partner_name': fields.function(get_partner_info, type='char', store=False, multi='partner_info'),
     }
 account_invoice()
 
 
 class invoice_line(osv.osv):
-    def _get_line_tax(self,cr,uid,ids,field,arg,context=None):
+    def _get_line_tax(self, cr, uid, ids, field, arg, context=None):
         res = {}
 
         for row in self.browse(cr, uid, ids, context):
@@ -82,7 +164,7 @@ class invoice_line(osv.osv):
 
         return res
 
-    def _get_tax_total(self,cr,uid,ids,field,arg,context=None):
+    def _get_tax_total(self, cr, uid, ids, field, arg, context=None):
         res = {}
 
         for row in self.browse(cr, uid, ids, context):
