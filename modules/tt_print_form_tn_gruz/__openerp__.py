ccb95db0ddc744724503ce79fe52a61aee6cb08b
Ivan Kondrashov <ivkond@gmail.com>
2013-10-31 18:19:27 +0400

    Добавил в зависимости модуль замены сохраняемых имён файлов. Добавил перевод.

--- modules/tt_print_form_tn_gruz/__openerp__.py
+++ modules/tt_print_form_tn_gruz/__openerp__.py
@@ -4,7 +4,7 @@
     'category': 'Extra Reports',
     'author': 'Transparent Technologies',
     'license': 'AGPL-3',
-    'depends': ['base', 'stock', 'jasper_reports', 'l10n_ru'],
+    'depends': ['base', 'stock', 'jasper_reports', 'l10n_ru', 'tt_print_forms_names'],
     'update_xml': [
         'tt_print_form_tn_gruz_data.xml',
         'tt_print_form_tn_gruz_view.xml',
