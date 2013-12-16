16e6b12db50bdee37f52a2f33f62fe9a5d1d3737
Ivan Kondrashov <ivkond@gmail.com>
2013-12-16 22:28:53 +0400

    Переименован объект bill_of_product_report в nakl_sklad_report в модуле печатной формы Накладная для склада.

--- modules/tt_print_form_nakl_sklad/report/nakl_sklad.py
+++ modules/tt_print_form_nakl_sklad/report/nakl_sklad.py
@@ -2,11 +2,11 @@ from openerp.report import report_sxw
 import time
 
 
-class bill_of_product_report(report_sxw.rml_parse):
+class nakl_sklad_report(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
-        super(bill_of_product_report, self).__init__(cr, uid, name, context=context)
+        super(nakl_sklad_report, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({'time': time})
 
 report_sxw.report_sxw('report.account.invoice.nakl_sklad', 'account.invoice',
                       'tt_print_form_nakl_sklad/nakl_sklad.jrxml',
-                      parser=bill_of_product_report)
+                      parser=nakl_sklad_report)
